184f5d083a692930a15171cea531843b0ea1fa14
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-11-26 13:39:32 +0100

    connect to mongoose and added example.env

--- server/app.js
+++ server/app.js
@@ -2,13 +2,34 @@ const express = require("express")
 const { graphqlHTTP } = require("express-graphql")
 const graphql = require("graphql")
 const schema = require("./schema/schema")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
 const app = express()
-
 const port = 4000
+
+dotenv.config({
+	path: "./config.env",
+})
 
 app.listen(port, () => {
 	console.log(`Listening to requests on port ${port}`)
 })
+
+//connect to mongodb
+const DB = process.env.DB
+mongoose
+	.connect(DB, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+	})
+	.then(() => {
+		console.log(`Running mongoose...`)
+	})
+	.catch((err) => {
+		console.log(err)
+	})
 
 //middleware for graphql
 app.use(
